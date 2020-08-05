0818fc5e326d82dd5011f223541269cee9f49011
Nicolas <nicolasm15@hotmail.com>
2020-08-05 17:26:17 -0300

    feat: add pages form and list

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,28 +1,48 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
+
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." >
+      <PageHeader title="Estes são os proffys disponíveis." >
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="subject"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="subject"/>
-          </div>
+          <Select 
+            name="subject" 
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Música', label: 'Música' },
+            ]} 
+          />
+          <Select 
+          name="week_day" 
+          label="Dia da semana"
+          options={[
+            { value: '0', label: 'Domingo' },
+            { value: '1', label: 'Segunda-feira' },
+            { value: '2', label: 'Terça-feira' },
+            { value: '3', label: 'Quarta-feira' },
+            { value: '4', label: 'Quinta-feira' },
+            { value: '5', label: 'Sexta-feira' },
+            { value: '6', label: 'Sábado' },
+          ]} 
+          />
+          <Input  type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
